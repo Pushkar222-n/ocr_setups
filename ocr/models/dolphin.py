51bc73e0123c67e101aa46b442512f333402a3ca
Pushkar Niroula <pushkarniroula@gmail.com>
2026-07-07 12:59:29 +0545

    QwenVL changes

--- ocr/models/dolphin.py
+++ ocr/models/dolphin.py
@@ -83,7 +83,7 @@ class DolphinModel(OCRModel):
         from transformers import (
             AutoProcessor,
             AutoTokenizer,
-            AutoModelForCausalLM,
+            Qwen2_5_VLForConditionalGeneration,
         )
 
         logger.info("Loading Dolphin from %s …", self.model_path)
@@ -109,7 +109,7 @@ class DolphinModel(OCRModel):
         )
 
         dtype = torch.float16 if self.device != "cpu" else torch.float32
-        self._model = AutoModelForCausalLM.from_pretrained( # <-- Change this class
+        self._model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
             self.model_path,
             torch_dtype=dtype,
             trust_remote_code=True,
